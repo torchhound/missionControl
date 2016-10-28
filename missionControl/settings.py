bc6e0f7b126e52853473bc1872695bf5b5430592
torchhound <jcieslik@umail.iu.edu>
2016-10-28 16:01:18 -0400

    Still working on serving static css

--- missionControl/settings.py
+++ missionControl/settings.py
@@ -118,4 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'missionControl/static/')
+STATIC_URL = "/static/"
+
+STATICFILES_DIR = [
+        os.path.join(BASE_DIR, "missionControl/static/")
+]
