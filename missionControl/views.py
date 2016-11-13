ad95e28d1ba24e8a9ea65774c4e1f06d5cb860a4
torchhound <jcieslik@umail.iu.edu>
2016-11-13 18:25:40 -0500

    Added POST code and API integration not working

--- missionControl/views.py
+++ missionControl/views.py
@@ -1,14 +1,33 @@
 from django.http import HttpResponse
 from django.template import Context, loader
+import requests
 
 def livingRoom(request):
-    template = loader.get_template("livingRoom.html")
-    return HttpResponse(template.render())
+	if request.method == "GET":
+		template = loader.get_template("livingRoom.html")
+		return HttpResponse(template.render())
+	elif request.POST.get("on", False):
+		requests.post("http://localhost:5000/livingroom", data = {"on":True})
+	elif request.POST.get("off", False):
+		requests.post("http://localhost:5000/livingroom", data = {"off":True})
+	elif request.POST.get("dim", False):
+		requests.post("http://localhost:5000/livingroom", data = {"dim":True})
+	elif request.POST.get("night", False):
+		requests.post("http://localhost:5000/livingroom", data = {"night":True})
 
 def kittyCorner(request):
-    template = loader.get_template("kittyCorner.html")
-    return HttpResponse(template.render())
-	
+	if request.method == "GET":
+		template = loader.get_template("kittyCorner.html")
+		return HttpResponse(template.render())
+	elif request.POST.get("on", False):
+		requests.post("http://localhost:5000/livingroom", data = {"on":True})
+	elif request.POST.get("off", False):
+		requests.post("http://localhost:5000/livingroom", data = {"off":True})
+	elif request.POST.get("dim", False):
+		requests.post("http://localhost:5000/livingroom", data = {"dim":True})
+	elif request.POST.get("night", False):
+		requests.post("http://localhost:5000/livingroom", data = {"night":True})
+
 def bathroom(request):
     template = loader.get_template("bathroom.html")
     return HttpResponse(template.render())
