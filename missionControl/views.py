2f2313c5d5b979e8d683d836377018e7c3f98e05
torchhound <jcieslik@umail.iu.edu>
2016-11-15 18:48:04 -0500

    Visual improvements, added gitignore, changed grammar in README

--- missionControl/views.py
+++ missionControl/views.py
@@ -6,9 +6,10 @@ def livingRoom(request):
 	if request.method == "GET":
 		template = loader.get_template("livingRoom.html")
 		return HttpResponse(template.render())
-	elif request.POST.get("on", False):
+	if request.GET.get("on"):
+		print("Living Room ON")
 		requests.post("http://localhost:5000/livingroom", data = {"on":True})
-	elif request.POST.get("off", False):
+	if request.POST.get("off", False):
 		requests.post("http://localhost:5000/livingroom", data = {"off":True})
 	elif request.POST.get("dim", False):
 		requests.post("http://localhost:5000/livingroom", data = {"dim":True})
